e7021e4ab203a0f1f8218d4bede23c75e5c10a36
Urii <105481784+UraGucal@users.noreply.github.com>
2022-10-07 11:02:30 +0300

    React modal first

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Button } from 'react-bootstrap';
 import './App.css';
 
 function App() {
+ const [open, setOpen] =  React.useState(false);
+ const modalTittle = 'Мотозапчасти';
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header"> 
+        <div  className="container bg-light">
+          <div className={`header-intro ${open ? 'show' : ''}`}>
+            <Button onClick={ ()=> setOpen(true) } className="btn-danger mb-3">Каталог товаров</Button>
+            {open &&(
+              <div className="overlay">
+                <div className="Vmodal">
+                  <div className="Vmodal-header">
+                    <h1>{modalTittle}</h1>
+                    <img onClick={ ()=> setOpen(false)} alt="lan" src="https://img.icons8.com/color/48/000000/close-window.png"/>
+                  </div>
+                </div>
+              </div>
+            )}
+          </div> 
+        </div>
       </header>
     </div>
   );
